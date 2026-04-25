69f66c14a6877a398709a29e7a574fca287526a2
旧故里小牛 <starrysea2312@yeah.net>
2026-04-25 11:00:32 +0800

    Delete src directory

--- src/api/account.js
+++
@@ -1,9 +0,0 @@
-import api from './index'
-
-export const accountAPI = {
-  getAll: () => api.get('/accounts'),
-  getById: (id) => api.get(`/accounts/${id}`),
-  create: (data) => api.post('/accounts', data),
-  update: (id, data) => api.put(`/accounts/${id}`, data),
-  delete: (id) => api.delete(`/accounts/${id}`)
-}
--- src/api/auth.js
+++
@@ -1,8 +0,0 @@
-import api from './index'
-
-export const authAPI = {
-  login: (credentials) => api.post('/auth/login', credentials),
-  register: (userData) => api.post('/auth/register', userData),
-  logout: () => api.post('/auth/logout'),
-  getCurrentUser: () => api.get('/auth/me')
-}
